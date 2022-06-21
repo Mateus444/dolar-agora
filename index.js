aa647bccc9a21bb474c3c5529f56cdb0c5b9a4f0
Mateus444 <62158989+Mateus444@users.noreply.github.com>
2022-06-21 19:23:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -41,15 +41,16 @@
   		}
 	  
 	  function criarTabela() {
-	    let multi = [2,10,20,25,30,50,100,150,1000,1500,2000,2500];
+	    let multi = [2,10,20,30,40,50,100,300,500,1000];
+            let escrito = ["(Dois dólares)", "(Dez dólares)","(Vinte dólares)", "(Trinta dólares)","(Quarenta dólares)","(Cinquenta dólares)","(Cem dólares)","(Trezentos dólares)","(Quinhentos dólares)", "(Mil dólares)"];
 	    let tbody = document.getElementById("tbody");
 	    tbody.innerText = "";
-	    for (let i = 0; i < 12; i++) {
+	    for (let i = 0; i < 10; i++) {
 	      let tr = tbody.insertRow();
 	      let td_cotacao = tr.insertCell();
 	      let td_real = tr.insertCell();
 	    
-	      td_cotacao.innerText = "US$"+" "+multi[i];
+	      td_cotacao.innerText = "US$"+" "+multi[i]+" "+escrito[i];
 	      let conversao = multi[i] * cotacaoFloat;
 	      td_real.innerText = conversao.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
 	    }
